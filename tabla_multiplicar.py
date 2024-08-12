df0a5bba4627c43196f85e298edf67a5446c558e
ramon <ramonguerraospino@hotmail.es>
2024-08-11 19:22:35 -0500

    tabla de multiplicar

---
+++ tabla_multiplicar.py
@@ -0,0 +1,10 @@
+
+print("--table of multiply--")
+
+#n= int(input("ingrese el numero:"))
+for a in range(1,10): #a cambia de valor 10 veces (en indice)
+    print("--tabla de multiplicar-",a)
+    for i in range(1,10): #a cambia de valor 10 veces (en indice)
+        t= a * i
+        print(a,"x",i,"=",t)
+
